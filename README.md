fb19bf981860dce3d04be8ff77e4ca354e6142db
Gareth Field <field.gareth@gmail.com>
2019-04-28 14:08:03 -0400

    colors! physical notes have some good stuff here

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 Good morning! Felt the need to have some fun and go back through the basics, so I'm going through w3schools again! Woohoo!
 
-2019-04-27 Good morning again! /// And afternoon! Evening!
+2019-04-27 Good morning again! /// And afternoon! // And good evening! Next up, when you're back to coding, clean that code up! Make it nice :)
+
+04-28 Good morning! Whoop, can't use that notation, forgot how to markdown. TODO!
+
+# TODO: Print out markdown cheatsheet! Have these things in the plastic things and in a binder!
+
+## Oh, shoot, that's headings, not comments. Lol!
+
+Aaaanyway, just read last nights' notes, and yessir! First step that yellow doesn't work on the background color I gave the site, though it's actually quite nice in dark mode.
--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ const Headers = () => (
 		<h1>This is an h1.</h1>
 		<h2>This is an h2.</h2>
 		<h3>This is an h3.</h3>
-		<h4 style={{fontFamily:'bembo'}}>This is an h4 in bembo.</h4>
-		<h5 style={{color:'#ff66fa'}}>This is a pink h5.</h5>
-		<h6 style={{ fontSize:'70px', marginBlockStart: '.1em', marginBlockEnd: '.1em' }}>This is an h6 with a font-size of 70px :)</h6>
+		<h4 style={{ fontFamily: 'bembo' }}>This is an h4 in bembo.</h4>
+		<h5 style={{ color: '#ff66fa' }}>This is a pink h5.</h5>
+		<h6 style={{ fontSize: '70px', marginBlockStart: '.1em', marginBlockEnd: '.1em' }}>
+			This is an h6 with a font-size of 70px :)
+		</h6>
 	</React.Fragment>
 );
 
@@ -36,29 +38,53 @@ function App() {
 		</React.Fragment>
 	);
 	return (
-		<div className='App' style={{backgroundColor:'#C4EEC4'}}>
-      <img title="I'm from the anime A Certain Scientific Railgun!" src={railgunImage} height='200px' width='auto' alt='a simple sheild logo from the A Certain Scientific Railgun anime series' style={{ padding: '20px' }}/>
+		<div className='App' style={{ backgroundColor: '#C4EEC4' }}>
+			<img
+				title='I&#39;m from the anime A Certain Scientific Railgun!'
+				src={railgunImage}
+				height='200px'
+				width='auto'
+				alt='a simple sheild logo from the A Certain Scientific Railgun anime series'
+				style={{ padding: '20px' }}
+			/>
 			<Headers />
 			<p>
-				<small>My life</small><br />
-				<em>you electrify my life</em><br />
-				<strong>let's conspire to reignite</strong><br />
-				<del>all the souls that would die just to feel alive!</del><br />
-				<mark>~Muse (Black Holes and Revelations)</mark><br />
+				<small>My life</small>
+				<br />
+				<em>you electrify my life</em>
+				<br />
+				<strong>let's conspire to reignite</strong>
+				<br />
+				<del>all the souls that would die just to feel alive!</del>
+				<br />
+				<mark>~Muse (Black Holes and Revelations)</mark>
+				<br />
 				<sub>P.S. This is how to use break tags to start new lines in a p tag!</sub>
 			</p>
-			<pre>
-				{
-	`Here's some preformatted text!
+			<pre style={{ border: '4px solid mediumpurple' }}>
+				{`Here's some preformatted text!
 		Are we tabbed over?
 		Tabs? ...       ... Those a thing?
-		Taaabs!`
-				}
+		Taaabs!`}
 			</pre>
-			<p>For a short quote, I described the Burundi Long Miles as, <q>getting face-****ed by an entire orchard of fruit.</q></p>
-			<blockquote cite="https://www.garethfield.com/fakeCitations">And for a longer quote I think I'll ramble for a bit. Hard to say this isn't a quote from me, really, as I'm the one typing it and not looking at anything or thinking of anything specifically. Thinking it'd be nice if my code formatter worked like it used to. Surely all this will make sense when it's put together, when it's on my big screen and in the context of everything being aligned center, it looks like another splotch of text. It's a blockquote, though, I swear!</blockquote>
-			<p>And while we're attributing things, if you use any abbreviations like <abbr title='National Aeronautics and Space Administration'>NASA</abbr> use an abbr tag.</p>
-			<p>And if we're to cite things like <cite>Fight Club</cite> by Chuck Palahniuk, please use a cite tag</p>
+			<p>
+				For a short quote, I described the Burundi Long Miles as,{' '}
+				<q>getting face-****ed by an entire orchard of fruit.</q>
+			</p>
+			<blockquote cite='https://www.garethfield.com/fakeCitations'>
+				And for a longer quote I think I'll ramble for a bit. Hard to say this isn't a quote from me, really, as I'm the
+				one typing it and not looking at anything or thinking of anything specifically. Thinking it'd be nice if my code
+				formatter worked like it used to. Surely all this will make sense when it's put together, when it's on my big
+				screen and in the context of everything being aligned center, it looks like another splotch of text. It's a
+				blockquote, though, I swear!
+			</blockquote>
+			<p>
+				And while we're attributing things, if you use any abbreviations like{' '}
+				<abbr title='National Aeronautics and Space Administration'>NASA</abbr> use an abbr tag.
+			</p>
+			<p>
+				And if we're to cite things like <cite>Fight Club</cite> by Chuck Palahniuk, please use a cite tag
+			</p>
 			<ol>
 				<li>This is a list.</li>
 				<li>A what?</li>
@@ -81,7 +107,7 @@ function App() {
 				New Largess, Vermont<br />
 				US
 			</address>
-			<HTMLComment text="Lesson learned, that HTML quotes are non-trivial in React!" />
+			<HTMLComment text='Lesson learned, that HTML quotes are non-trivial in React!' />
 		</div>
 	);
 }
@@ -107,30 +133,57 @@ function AppInDarkMode() {
 		</React.Fragment>
 	);
 	return (
-		<div className='App' style={{color: '#c4eec4', backgroundColor:'#16161d'}}>
-      <img title="I'm from the anime A Certain Scientific Railgun!" src={railgunImage} height='200px' width='auto' alt='a simple sheild logo from the A Certain Scientific Railgun anime series' style={{ padding: '20px' }}/>
+		<div className='App' style={{ color: '#c4eec4', backgroundColor: '#16161d' }}>
+			<img
+				title='I&#39;m from the anime A Certain Scientific Railgun!'
+				src={railgunImage}
+				height='200px'
+				width='auto'
+				alt='a simple sheild logo from the A Certain Scientific Railgun anime series'
+				style={{ padding: '20px' }}
+			/>
 			<Headers />
 			<p>
-				<small>My life</small><br />
-				<em>you electrify my life</em><br />
-				<strong>let's conspire to reignite</strong><br />
-				<del>all the souls that would die just to feel alive!</del><br />
-				<mark>~Muse (Black Holes and Revelations)</mark><br />
+				<small>My life</small>
+				<br />
+				<em>you electrify my life</em>
+				<br />
+				<strong>let's conspire to reignite</strong>
+				<br />
+				<del>all the souls that would die just to feel alive!</del>
+				<br />
+				<mark>~Muse (Black Holes and Revelations)</mark>
+				<br />
 				<sub>P.S. This is how to use break tags to start new lines in a p tag!</sub>
 			</p>
-			<pre>
-				{
-	`Here's some preformatted text!
+			<pre style={{ border: '4px solid mediumpurple' }}>
+				{`Here's some preformatted text!
 		Are we tabbed over?
 		Tabs? ...       ... Those a thing?
-		Taaabs!`
-				}
+		Taaabs!`}
 			</pre>
-			<p>For a short quote, I described the Burundi Long Miles as, <q>getting face-****ed by an entire orchard of fruit.</q></p>
-			<blockquote cite="https://www.garethfield.com/fakeCitations">And for a longer quote I think I'll ramble for a bit. Hard to say this isn't a quote from me, really, as I'm the one typing it and not looking at anything or thinking of anything specifically. Thinking it'd be nice if my code formatter worked like it used to. Surely all this will make sense when it's put together, when it's on my big screen and in the context of everything being aligned center, it looks like another splotch of text. It's a blockquote, though, I swear! Oh actually though in the HTML spec it says how it should be used and this is not it.</blockquote>
-			<a href='https://www.w3.org/TR/2011/WD-html5-author-20110705/the-blockquote-element.html' target='_blank'>The spec for blockquote tag.</a>
-			<p>And while we're attributing things, if you use any abbreviations like <abbr title='National Aeronautics and Space Administration'>NASA</abbr> use an abbr tag.</p>
-			<p>And if we're to cite things like <cite>Fight Club</cite> by Chuck Palahniuk, please use a cite tag</p>
+			<p>
+				For a short quote, I described the Burundi Long Miles as,{' '}
+				<q>getting face-****ed by an entire orchard of fruit.</q>
+			</p>
+			<blockquote cite='https://www.garethfield.com/fakeCitations'>
+				And for a longer quote I think I'll ramble for a bit. Hard to say this isn't a quote from me, really, as I'm the
+				one typing it and not looking at anything or thinking of anything specifically. Thinking it'd be nice if my code
+				formatter worked like it used to. Surely all this will make sense when it's put together, when it's on my big
+				screen and in the context of everything being aligned center, it looks like another splotch of text. It's a
+				blockquote, though, I swear! Oh actually though in the HTML spec it says how it should be used and this is not
+				it.
+			</blockquote>
+			<a href='https://www.w3.org/TR/2011/WD-html5-author-20110705/the-blockquote-element.html' target='_blank'>
+				The spec for blockquote tag.
+			</a>
+			<p>
+				And while we're attributing things, if you use any abbreviations like{' '}
+				<abbr title='National Aeronautics and Space Administration'>NASA</abbr> use an abbr tag.
+			</p>
+			<p>
+				And if we're to cite things like <cite>Fight Club</cite> by Chuck Palahniuk, please use a cite tag
+			</p>
 			<ol>
 				<li>This is a list.</li>
 				<li>A what?</li>
@@ -153,7 +206,7 @@ function AppInDarkMode() {
 				New Largess, Vermont<br />
 				US
 			</address>
-			<HTMLComment text="Lesson learned, that HTML quotes are non-trivial in React!" />
+			<HTMLComment text='Lesson learned, that HTML quotes are non-trivial in React!' />
 		</div>
 	);
 }
